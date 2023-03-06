47424d53163340f56d2fad6fe9a7e15417ee6b2d
Eduardo <user_mrttgvtef311@tribanco.com.br>
2023-03-06 14:19:23 -0300

    First upload project

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# java-spring-api-parking-control
--
+## java-spring-api-parking-control
+
+### Description
+
+- Java project.
+
+### Technologies
+
+- Java 19
+- Spring 3.0.4
+
+### Instalation
+
+- Clone this project.
+- Open the Eclipse or STS and go to import option, choose General > Existing Projects into Workspace
+- Build the project with parameter "clean package install"
+- Install Postgres server, create the
+- Run the Main File.
+
+
+
+### Image
+
+<p align="center">
+  <img src="">
+</p>
+
---
+++ parkingcontrol/src/main/java/com/parkingcontrol/services/ParkingSpotService.java
@@ -0,0 +1,22 @@
+package com.parkingcontrol.services;
+
+import com.parkingcontrol.models.ParkingSpotModel;
+import com.parkingcontrol.repositories.ParkingSpotRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ParkingSpotService {
+
+    final ParkingSpotRepository parkingSpotRepository;
+
+    public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
+        this.parkingSpotRepository = parkingSpotRepository;
+    }
+
+    @Transactional
+    public ParkingSpotModel save(ParkingSpotModel parkingSpotModel)
+    {
+        return parkingSpotRepository.save(parkingSpotModel);
+    }
+}
